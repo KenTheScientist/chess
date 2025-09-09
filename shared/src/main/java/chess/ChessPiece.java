f6bf86a14461926b5af18699f491cf2843a4cc7f
Kenneth Thomson <ken.d.thomson@gmail.com>
2025-09-09 15:42:05 -0600

    (p0) Create equals functions & bishop moves

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,7 +1,7 @@
 package chess;
 
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
+import java.util.ArrayList;
 
 /**
  * Represents a single chess piece
@@ -15,9 +15,23 @@ public class ChessPiece {
     private final ChessGame.TeamColor pieceColor;
     private final PieceType type;
 
-    public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+    public ChessPiece(ChessGame.TeamColor pieceColor, PieceType type) {
         this.pieceColor = pieceColor;
         this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPiece that = (ChessPiece) o;
+        return pieceColor == that.pieceColor && type == that.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceColor, type);
     }
 
     /**
@@ -54,6 +68,96 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        return List.of(); //FIX THIS!
+
+        Collection<ChessPosition> possiblePositions = new ArrayList<ChessPosition>(); //List of possible move-to positions
+        Collection<ChessMove> possibleMoves = new ArrayList<ChessMove>(); //Output list of ChessMoves
+
+        int myCol = myPosition.col;
+        int myRow = myPosition.row;
+
+        switch (type){
+            case PieceType.BISHOP:
+                //Go up and right
+                int checkingCol = myCol;
+                int checkingRow = myRow;
+
+                while(true)//UP AND RIGHT
+                {
+                    checkingRow++;
+                    checkingCol++;
+                    if(checkingCol > 8 || checkingRow > 8){
+                        break;
+                    }
+                    else{
+                        possiblePositions.add(new ChessPosition(checkingRow, checkingCol));
+                        if(board.hasPiece(new ChessPosition(checkingRow, checkingCol))){
+                            break;
+                        }
+                    }
+                }
+
+                checkingCol = myCol;
+                checkingRow = myRow;
+                while(true)//UP AND LEFT
+                {
+                    checkingRow++;
+                    checkingCol--;
+                    if(checkingCol < 1 || checkingRow > 8){
+                        break;
+                    }
+                    else{
+                        possiblePositions.add(new ChessPosition(checkingRow, checkingCol));
+                        if(board.hasPiece(new ChessPosition(checkingRow, checkingCol))){
+                            break;
+                        }
+                    }
+                }
+
+                checkingCol = myCol;
+                checkingRow = myRow;
+                while(true)//DOWN AND RIGHT
+                {
+                    checkingRow--;
+                    checkingCol++;
+                    if(checkingCol > 8 || checkingRow < 1){
+                        break;
+                    }
+                    else{
+                        possiblePositions.add(new ChessPosition(checkingRow, checkingCol));
+                        if(board.hasPiece(new ChessPosition(checkingRow, checkingCol))){
+                            break;
+                        }
+                    }
+                }
+
+                checkingCol = myCol;
+                checkingRow = myRow;
+                while(true)//DOWN AND LEFT
+                {
+                    checkingRow--;
+                    checkingCol--;
+                    if(checkingCol < 1 || checkingRow < 1){
+                        break;
+                    }
+                    else{
+                        possiblePositions.add(new ChessPosition(checkingRow, checkingCol));
+                        if(board.hasPiece(new ChessPosition(checkingRow, checkingCol))){
+                            break;
+                        }
+                    }
+                }
+
+
+                break;
+        }
+
+
+        //Now fill the possibleMoves collection using the possible positions
+        for(ChessPosition pos : possiblePositions){
+            possibleMoves.add(new ChessMove(myPosition, pos, null));
+        }
+
+        return possibleMoves;
+
     }
 }
