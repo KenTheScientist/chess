65c065d1da3690eee5cff1ab82ba3070978dccbf
Kenneth Thomson <ken.d.thomson@gmail.com>
2025-11-06 22:25:46 -0700

    (p5) Created BoardRenderer

---
+++ client/src/main/java/ui/BoardRenderer.java
@@ -0,0 +1,15 @@
+package ui;
+
+import chess.ChessBoard;
+
+public class BoardRenderer {
+
+    public static String render(ChessBoard board, String ownerColor) {
+        String out = "urmom";
+
+
+
+        return out;
+    }
+
+}
