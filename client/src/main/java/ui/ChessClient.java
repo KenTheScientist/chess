685820f61eab221a483dbc61f696ac1c2f43dd36
Kenneth Thomson <ken.d.thomson@gmail.com>
2025-11-06 22:25:39 -0700

    (p5) Completed all ChessClient steps

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -1,5 +1,6 @@
 package ui;
 
+import chess.ChessGame;
 import chess.ResponseException;
 import chess.server.ClientCreateGameResult;
 import chess.server.ClientGameData;
@@ -13,13 +14,17 @@ public class ChessClient {
     private final ServerFacade serverFacade;
     private String authToken;
     private int currentGameID;
+    private ChessGame currentGame = new ChessGame();
+    private String currentColor = "WHITE";
 
     public enum State {
         SIGNEDOUT,
-        SIGNEDIN
+        SIGNEDIN,
+        INGAME,
+        OBSERVING
     }
 
-    private State state = State.SIGNEDOUT;
+    private State state = State.INGAME;
 
     public ChessClient(String serverUrl) {
         serverFacade = new ServerFacade(serverUrl);
@@ -32,25 +37,42 @@ public class ChessClient {
         Scanner scanner = new Scanner(System.in);
         var result = "";
         while(!result.equals("quit")){
-            System.out.print(">>>");
-            String line = scanner.nextLine();
-            try{
-                result = eval(line);
-                System.out.println(result);
-            } catch (Throwable e)
-            {
-                var msg = e.toString();
-                System.out.print(msg);
-                e.printStackTrace();
-                System.out.print("\n");
+            if(state == State.SIGNEDOUT || state == State.SIGNEDIN) {
+                System.out.print(">>>");
+                String line = scanner.nextLine();
+                try {
+                    result = eval(line);
+                    System.out.println(result);
+                } catch (Throwable e) {
+                    var msg = e.toString();
+                    System.out.print(msg);
+                    System.out.print("\n");
+                }
+            }
+            else if(state == State.INGAME){
+                System.out.print(BoardRenderer.render(currentGame.gameBoard, currentColor));
+                System.out.print("\n>>>");
+                String line = scanner.nextLine();
+                try {
+
+                    System.out.println(line);
+                } catch (Throwable e) {
+                    var msg = e.toString();
+                    System.out.print(msg);
+                    System.out.print("\n");
+                }
             }
 
         }
     }
 
+    //Game functions
+
+
     // Command functions
     public String register(String[] params) throws ResponseException {
         //register(username, password, email)
+        assertSignedOut();
         if(params.length >= 3) {
             var loginResult = serverFacade.register(params[0], params[1], params[2]);
             state = State.SIGNEDIN;
@@ -64,6 +86,7 @@ public class ChessClient {
     }
     public String login(String[] params) throws ResponseException {
         //login(username, password)
+        assertSignedOut();
         if(params.length >= 2) {
             var loginResult = serverFacade.login(params[0], params[1]);
             state = State.SIGNEDIN;
@@ -78,6 +101,7 @@ public class ChessClient {
 
     public String listGames() throws ResponseException {
         //listgames()
+        assertSignedIn();
         var listGamesResult = serverFacade.listGames(authToken);
         StringBuilder out = new StringBuilder();
         var list = listGamesResult.games;
@@ -97,12 +121,14 @@ public class ChessClient {
 
     public String playGame(String[] params) throws ResponseException {
         //playGame(gameNumber, color)
+        assertSignedIn();
         if(params.length >= 2) {
             var listGameResult = serverFacade.listGames(authToken);
             int searchingIndex = Integer.parseInt(params[0])-1;
             ClientGameData foundGameData = listGameResult.games.get(searchingIndex);
             serverFacade.joinGame(authToken,params[1].toUpperCase(),foundGameData.gameID());
             currentGameID = foundGameData.gameID();
+            state = State.INGAME;
             return String.format("Successfully joined game %s as %s!",
                     foundGameData.gameName(), params[1].toUpperCase());
 
@@ -114,6 +140,7 @@ public class ChessClient {
 
     public String createGame(String[] params) throws ResponseException {
         //createGame(gameName)
+        assertSignedIn();
         if(params.length >= 1) {
             ClientCreateGameResult result = serverFacade.createGame(params[0],authToken);
             return String.format("Successfully created game %s", params[0]);
@@ -123,6 +150,25 @@ public class ChessClient {
         }
     }
 
+    public String observeGame(String[] params) throws ResponseException {
+        //createGame(gameName)
+        assertSignedIn();
+        if(params.length >= 1) {
+            ClientCreateGameResult result = serverFacade.createGame(params[0],authToken);
+            state = State.OBSERVING;
+            return String.format("Successfully observing game %s", params[0]);
+        }
+        else {
+            throw new ResponseException(ResponseException.Code.ClientError, "Expected: <GAME NAME>");
+        }
+    }
+
+    public String logout() throws ResponseException {
+        assertSignedIn();
+        state = State.SIGNEDOUT;
+        authToken = null;
+        return "Successfully signed out.";
+    }
 
 
 
@@ -143,8 +189,8 @@ public class ChessClient {
                 case "listgames" -> listGames();
                 case "playgame" -> playGame(params);
                 case "creategame" -> createGame(params);
-                case "observegame" -> help();
-                case "logout" -> help();
+                case "observegame" -> observeGame(params);
+                case "logout" -> logout();
 
                 default -> help();
             };
@@ -164,11 +210,25 @@ public class ChessClient {
         }
         return """
                 - listgames - get a list of active games
-                - playgame - join an online game
-                - creategame - create a new online game
+                - playgame <GAME NUMBER> <PLAYER COLOR> - join an online game
+                - creategame <GAME NAME> - create a new online game
                 - observegame - observe a game
                 - logout - log out of account
                 - help - get help with possible commands
                 """;
     }
+
+    private void assertSignedIn() throws ResponseException {
+        if (state == State.SIGNEDOUT) {
+            throw new ResponseException(ResponseException.Code.ClientError, "You must sign in");
+        }
+    }
+
+    private void assertSignedOut() throws ResponseException {
+        if (state != State.SIGNEDOUT) {
+            throw new ResponseException(ResponseException.Code.ClientError, "You must be sign out first");
+        }
+    }
+
+
 }
